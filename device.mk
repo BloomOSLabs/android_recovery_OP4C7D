254373754dab6e20d2665a6ebf9628e7824b24c1
stanlysilas <vstanlysilas@gmail.com>
2026-05-16 21:59:25 +0530

    Updated file(s)

--- device.mk
+++ device.mk
@@ -1,6 +1,7 @@
 PRODUCT_COPY_FILES += \
 $(LOCAL_PATH)/rootdir/init.recovery.mt6765.rc:root/init.recovery.mt6765.rc \
-$(LOCAL_PATH)/rootdir/system/etc/fstab.mt6765:root/system/etc/fstab.mt6765
+$(LOCAL_PATH)/rootdir/system/etc/fstab.mt6765:root/system/etc/fstab.mt6765 \
+$(LOCAL_PATH)/system.prop:system/system.prop
 
 PRODUCT_PACKAGES += \
 recovery \
@@ -10,3 +11,6 @@ adbd \
 fastbootd
 
 PRODUCT_CHARACTERISTICS := nosdcard
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.treble.enabled=true
