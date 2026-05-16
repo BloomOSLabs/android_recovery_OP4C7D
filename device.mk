13b52fdbd71d32f130314cef90aeb712cbf368a2
stanlysilas <vstanlysilas@gmail.com>
2026-05-16 20:28:14 +0530

    Updated file(s)

--- device.mk
+++ device.mk
@@ -1,12 +1,12 @@
-PRODUCT_COPY_FILES += 
-$(LOCAL_PATH)/rootdir/init.recovery.mt6765.rc:root/init.recovery.mt6765.rc 
+PRODUCT_COPY_FILES += \
+$(LOCAL_PATH)/rootdir/init.recovery.mt6765.rc:root/init.recovery.mt6765.rc \
 $(LOCAL_PATH)/rootdir/system/etc/fstab.mt6765:root/system/etc/fstab.mt6765
 
-PRODUCT_PACKAGES += 
-recovery 
-toybox 
-toolbox 
-adbd 
+PRODUCT_PACKAGES += \
+recovery \
+toybox \
+toolbox \
+adbd \
 fastbootd
 
 PRODUCT_CHARACTERISTICS := nosdcard
