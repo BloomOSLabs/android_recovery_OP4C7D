2be318c518b31d32d2fb6123013729cee43db996
stanlysilas <vstanlysilas@gmail.com>
2026-05-16 13:54:28 +0530

    Modified lineage_OP4C7Drecovery.mk

--- lineage_OP4C7Drecovery.mk
+++ lineage_OP4C7Drecovery.mk
@@ -6,7 +6,11 @@ PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := OP4C7D
 PRODUCT_MANUFACTURER := OPPO
 
-$(call inherit-product, build/target/product/base.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=OP4C7Drecovery \
+    PRODUCT_NAME=lineage_OP4C7Drecovery
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.recovery.mt6765.rc:root/init.recovery.mt6765.rc \
