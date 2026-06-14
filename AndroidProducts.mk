c5dcc98611a766ebb0354c7e8b243fc6be0f1b26
stanlysilas <vstanlysilas@gmail.com>
2026-06-14 16:03:43 +0530

    Added TWRP flags in BoardConfig.mk and migrated to TWRP from LineageOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_OP4C7D.mk
+    $(LOCAL_DIR)/omni_OP4C7D.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_OP4C7D-eng
+    omni_OP4C7D-eng \
+    omni_OP4C7D-userdebug \
+    omni_OP4C7D-user
--- omni_OP4C7D.mk
+++ omni_OP4C7D.mk
@@ -4,19 +4,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-$(call inherit-product, vendor/lineage/config/common.mk)
-
 PRODUCT_DEVICE := OP4C7D
-PRODUCT_NAME := lineage_OP4C7D
+PRODUCT_NAME := omni_OP4C7D
 PRODUCT_BRAND := OPPO
-PRODUCT_MODEL := OP4C7D
+PRODUCT_MODEL := CPH2015
 PRODUCT_MANUFACTURER := OPPO
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=OP4C7D
-    
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.version=BLoomOS-Recovery-16.0-Stanly-20260518
-    
+
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.build.display.id=BloomOS-Recovery
+    ro.build.display.id=TWRP-OP4C7D \
+    ro.product.device=OP4C7D
