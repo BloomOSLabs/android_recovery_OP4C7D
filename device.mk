1863aa40a3e4aee10cbdddfa0c01a93c4d9af12a
stanlysilas <vstanlysilas@gmail.com>
2026-06-16 19:22:06 +0530

    Add OF variables in vendorsetup.sh

--- device.mk
+++ device.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := device/oppo/OP4C7D
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/init.recovery.mt6765.rc:root/init.recovery.mt6765.rc \
-    $(LOCAL_PATH)/recovery/root/init.recovery.usb.rc:root/init.recovery.usb.rc \
+    # $(LOCAL_PATH)/recovery/root/init.recovery.usb.rc:root/init.recovery.usb.rc \
     $(LOCAL_PATH)/rootdir/system/etc/fstab.mt6765:root/system/etc/fstab.mt6765 \
     $(LOCAL_PATH)/system.prop:system/system.prop \
     $(LOCAL_PATH)/rootdir/vendor/build.prop:root/vendor/build.prop \
