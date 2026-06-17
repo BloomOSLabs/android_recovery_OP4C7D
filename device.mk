3c2bf0a7224b9603cbd06f294437dc57b029fa5b
stanlysilas <vstanlysilas@gmail.com>
2026-06-17 17:29:45 +0530

    Updated OrangeFox 12.1 variables

--- device.mk
+++ device.mk
@@ -2,13 +2,10 @@ LOCAL_PATH := device/oppo/OP4C7D
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/init.recovery.mt6765.rc:root/init.recovery.mt6765.rc \
-    # $(LOCAL_PATH)/recovery/root/init.recovery.usb.rc:root/init.recovery.usb.rc \
+    $(LOCAL_PATH)/recovery/root/init.recovery.usb.rc:root/init.recovery.usb.rc \
     $(LOCAL_PATH)/rootdir/system/etc/fstab.mt6765:root/system/etc/fstab.mt6765 \
     $(LOCAL_PATH)/system.prop:system/system.prop \
     $(LOCAL_PATH)/rootdir/vendor/build.prop:root/vendor/build.prop \
-    # $(LOCAL_PATH)/rootdir/vendor/manifest.xml:root/vendor/manifest.xml \
-    # $(LOCAL_PATH)/rootdir/vendor/compatibility_matrix.xml:root/vendor/compatibility_matrix.xml \
-    # $(LOCAL_PATH)/rootdir/system_root/system/compatibility_matrix.xml:root/system_root/system/compatibility_matrix.xml \
     $(LOCAL_PATH)/rootdir/vendor/lib64/libkeymaster3device.so:root/vendor/lib64/libkeymaster3device.so \
     $(LOCAL_PATH)/rootdir/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl.so:root/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl.so \
     $(LOCAL_PATH)/rootdir/vendor/lib64/hw/android.hardware.keymaster@3.0-impl.so:root/vendor/lib64/hw/android.hardware.keymaster@3.0-impl.so \
@@ -19,8 +16,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/sbin/keystore.mt6765.so:root/sbin/keystore.mt6765.so \
     $(LOCAL_PATH)/recovery/root/sbin/libTEECommon.so:root/sbin/libTEECommon.so \
     $(LOCAL_PATH)/recovery/root/sbin/libpuresoftkeymasterdevice.so:root/sbin/libpuresoftkeymasterdevice.so \
-    # $(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/vendor,root/vendor) \
-    # $(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/system_root,root/system_root) \
     $(LOCAL_PATH)/recovery/root/sbin/android.hardware.gatekeeper@1.0-service:root/sbin/android.hardware.gatekeeper@1.0-service \
     $(LOCAL_PATH)/recovery/root/sbin/android.hardware.keymaster@3.0-service:root/sbin/android.hardware.keymaster@3.0-service \
     $(LOCAL_PATH)/recovery/root/sbin/vendor.oppo.hardware.cryptoeng@1.0-service:root/sbin/vendor.oppo.hardware.cryptoeng@1.0-service \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,5 +7,17 @@ export FOX_BUILD_TYPE="Experimental"
 export OF_MAINTAINER="Stanly Silas"
 export FOX_MAINTAINER_PATCH_VERSION=1
 
+# Battery
+export OF_USE_LEGACY_BATTERY_SERVICES=1
+
+# Flashlight
+export OF_FL_PATH1="/sys/class/leds/flashlight/brightness"
+
+# Timezone
+OF_DEFAULT_TIMEZONE="IST-5:30"
+
 # Treble
 export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+
+# Miscellaneous
+export FOX_ALLOW_EARLY_SETTINGS_LOAD=1
