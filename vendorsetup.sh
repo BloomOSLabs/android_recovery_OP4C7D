0486bf51dc037521d3ac9b6f9163ca193d69687d
stanlysilas <vstanlysilas@gmail.com>
2026-06-17 13:40:10 +0530

    Updated files

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ echo "VENDORSETUP LOADED"
 # Build details
 export FOX_BUILD_TYPE="Experimental"
 export OF_MAINTAINER="Stanly Silas"
-export FOX_VERSION="12.1"
+export FOX_MAINTAINER_PATCH_VERSION="01"
 
 # Treble
 export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
