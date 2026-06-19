d742fb11fee6556949443bcb18928b91cdd89a77
stanlysilas <vstanlysilas@gmail.com>
2026-06-19 19:57:00 +0530

    Added more Crypto binaries

--- device.mk
+++ device.mk
@@ -7,31 +7,22 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system.prop:system/system.prop \
     $(LOCAL_PATH)/rootdir/vendor/build.prop:root/vendor/build.prop \
     $(LOCAL_PATH)/rootdir/ueventd.mt6765.rc:root/ueventd.mt6765.rc \
-    $(LOCAL_PATH)/rootdir/vendor/lib/libMcClient.so:root/vendor/lib/libMcClient.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib/libTEECommon.so:root/vendor/lib/libTEECommon.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib/libkeymaster3device.so:root/vendor/lib/libkeymaster3device.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib/vendor.oppo.hardware.commondcs@1.0.so:root/vendor/lib/vendor.oppo.hardware.commondcs@1.0.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib/hw/android.hardware.gatekeeper@1.0-impl.so:root/vendor/lib/hw/android.hardware.gatekeeper@1.0-impl.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib/hw/android.hardware.keymaster@3.0-impl.so:root/vendor/lib/hw/android.hardware.keymaster@3.0-impl.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib/hw/gatekeeper.mt6765.so:root/vendor/lib/hw/gatekeeper.mt6765.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib/hw/keystore.mt6765.so:root/vendor/lib/hw/keystore.mt6765.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib64/libMcClient.so:root/vendor/lib64/libMcClient.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib64/libTEECommon.so:root/vendor/lib64/libTEECommon.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib64/libkeymaster3device.so:root/vendor/lib64/libkeymaster3device.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib64/libtrustonic_keybox_ca.so:root/vendor/lib64/libtrustonic_keybox_ca.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib64/vendor.oppo.hardware.commondcs@1.0.so:root/vendor/lib64/vendor.oppo.hardware.commondcs@1.0.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib64/vendor.oppo.hardware.cryptoeng@1.0.so:root/vendor/lib64/vendor.oppo.hardware.cryptoeng@1.0.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl.so:root/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib64/hw/android.hardware.keymaster@3.0-impl.so:root/vendor/lib64/hw/android.hardware.keymaster@3.0-impl.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib64/hw/gatekeeper.mt6765.so:root/vendor/lib64/hw/gatekeeper.mt6765.so \
-    $(LOCAL_PATH)/rootdir/vendor/lib64/hw/keystore.mt6765.so:root/vendor/lib64/hw/keystore.mt6765.so \
-    $(LOCAL_PATH)/rootdir/vendor/bin/hw/android.hardware.gatekeeper@1.0-service:root/vendor/bin/hw/android.hardware.gatekeeper@1.0-service \
-    $(LOCAL_PATH)/rootdir/vendor/bin/hw/android.hardware.keymaster@3.0-service:root/vendor/bin/hw/android.hardware.keymaster@3.0-service \
-    $(LOCAL_PATH)/rootdir/vendor/bin/hw/vendor.oppo.hardware.cryptoeng@1.0-service:root/vendor/bin/hw/vendor.oppo.hardware.cryptoeng@1.0-service \
-    $(LOCAL_PATH)/rootdir/vendor/etc/init/vendor.oppo.hardware.cryptoeng@1.0-service.rc:root/vendor/etc/init/vendor.oppo.hardware.cryptoeng@1.0-service.rc \
-    $(LOCAL_PATH)/rootdir/vendor/etc/permissions/vendor-oppo-hardware.cryptoeng.xml:root/vendor/etc/permissions/vendor-oppo-hardware.cryptoeng.xml \
-    $(LOCAL_PATH)/recovery/root/sbin/libpuresoftkeymasterdevice.so:root/sbin/libpuresoftkeymasterdevice.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libpuresoftkeymasterdevice.so:root/sbin/libpuresoftkeymasterdevice.so
+
+PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/vendor/app,root/vendor/app)
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/vendor/bin,root/vendor/bin)
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/vendor/etc,root/vendor/etc)
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/vendor/lib,root/vendor/lib)
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/vendor/lib64,root/vendor/lib64)
 
 PRODUCT_PACKAGES += \
     recovery \
